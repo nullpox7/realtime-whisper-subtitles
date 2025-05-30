270b1ff5f76e195dc721664ffa71866ef7b3ad0e
nullpox7 <n.tsuboi@kumo-ichimonji.net>
2025-05-30 20:40:32 +0900

    Add test package initialization with UTF-8 encoding

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Test Package for Real-time Whisper Subtitles
+Test package initialization
+"""
+
+__version__ = "1.0.0"
+__description__ = "Test package for Real-time Whisper Subtitles system"
